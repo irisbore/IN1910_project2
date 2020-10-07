d46ccb909382ac7093bf583e2b8cd4437bad95e5
Iris Bore <irissb@uio.no>
2020-10-07 12:31:17 +0200

    changed constructor, added methods

--- linked_list.cpp
+++ linked_list.cpp
@@ -9,38 +9,79 @@ struct Node {
     Node* next;
 };
 
-//Constructor
-Node(int k, Node* p, Node* n) {
-    value = k;
-    previous = p;
-    next = n;
-}
-
 //Class
 class LinkedList {
-  private:
-    //If empty list point to nullptr
-    head = nullptr;
 
+  private:
     //Two node pointers
     Node* head;
     Node* tail;
+    size = 0;
+
 
   public:
-    int length() {
-    Node* current = head;
-    int count = 0;
-    while (current != nullptr) {
-        count++;
-        current = current->next;
+    //Constructor
+    LinkedList() {
+      //Assigning random value 0 to head and tail
+      head = new Node(0, nullptr, nullptr);
+      tail = new Node(0, nullptr, nullptr);
+      head->next = tail;
+      tail->previous = head;
       }
-      return count;
+
+    int length() {
+    return size;
+  }
+
+void append(int val){
+  Node* a = new Node(val, tail->previous, tail);
+  tail->previous = a;
+  a->previous->next = a;
+  size++;
+  }
+
+void print() {
+    Node* current = head->next;
+    cout << "[";
+    while (current->next != nullptr) {
+        cout << current->value;
+        cout << ", ";
+        current = current->next;
+    }
+    //while (current != tail:)
+    cout << current->value << "]" << endl;
+}
+
+~LinkedList() {
+    Node* current;
+    Node* previous;
+    Node* next;
+
+    current = head;
+
+    while (current != nullptr) {
+        previous = current->previous;
+        next = current->next;
+        delete current;
+        current = previous;
+        current = next;
     }
 
-void append(int){}
 
+int& operator[] (int index) {
+  Node* current = head->next;
 
-void print(){}
+  for (int i=0; i<index; i++) {
+    current = current->next;
+    }
+    return current;
+    }
 
+}
+void insert(int val, int index) {
+    Node* previous = get_node(index-1);
+    Node* next = prev->next;
+    prev->next = new Node(val, next);
+}
 
 };
