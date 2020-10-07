2212963cd1db66fe7bf0c3001b2e66af9fdded70
alice <alicemj@uio.no>
2020-10-07 12:38:36 +0200

    Added insert-method in 1g) and remove-method in 1h)

--- array_list.cpp
+++ array_list.cpp
@@ -1,7 +1,9 @@
 //#include <string>
 #include <iostream>
 #include <cassert>
-#include <vector>       
+#include <vector>
+#include  <stdexcept>
+
 //#include <cstdio>
 
 class ArrayList {
@@ -54,13 +56,53 @@ class ArrayList {
         delete[] x;
         x = tmp;
     }
+
     public:
-    void print(){ //Hvordan får man den til å printe
+    void print(){
         for (int i=0; i<size; i++){
 		    std::cout << x[i] << std::endl;
         }
-    };
+    }
+
+    int& operator[](int i){
+        if (i>=size){
+            throw std::out_of_range("Index is out of range");
+        }
+        else{
+            return x[i];
+        }
+    }
+
+    void insert(int val, int index){ 
+        if (index > size){
+            throw std::out_of_range("Index is out of range");
+        }
+        else if (index == size){
+            append(val);
+        }
+        else{
+            append(x[size]);
+            for (int i=size; i>index; i--){
+                x[i] = x[i-1];
+            }
+            x[index] = val;
+        }
+    }
+
+    void remove(int index){
+        if (index > size){
+            throw std::out_of_range("Index is out of range");
+        }
+        else{
+            size = size - 1; 
+            for (int i=index; i<size; i++){
+                x[i] = x[i+1]; 
+            } 
+        }
+    }
 };
+
+
 bool is_prime(int n){
     if (n == 1) {
         return false;
@@ -88,9 +130,14 @@ void test_is_prime(){
 
 int main(){
     ArrayList a;
-    a.print();
-    test_is_prime();
+    //a.print();
+    //test_is_prime();
     ArrayList primes({2, 3, 5, 8, 11});
+    //primes.print();
+    primes.insert(4, 2);
+    //primes.print();
+    primes.remove(2);
     primes.print();
+    //std::cout << primes[4] << std::endl;
     return 0;
 }
