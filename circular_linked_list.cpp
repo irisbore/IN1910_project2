eb87232a75a3d6b3e80b21e921817b78ea0d9de5
alice <alicemj@uio.no>
2020-10-09 11:30:31 +0200

    Added print in 4c) and added code for 4d) and 4e). Problem with add.

--- circular_linked_list.cpp
+++ circular_linked_list.cpp
@@ -20,16 +20,32 @@ struct Node {
 class  CircLinkedList{
     private:
     Node* head;
+    Node* tail;
     int size = 0;
 
     public:
     CircLinkedList() {
         head = nullptr;
     }
-
+/*
+    CircLinkedList(int n){
+        int i = 1;
+        if (head == nullptr) {
+            head = new Node(i);
+            size ++;
+        }
+        Node* current;
+        current = head;
+        while (i<=n){
+            current->next = new Node(i);
+            current = current->next;
+            size ++;
+        }
+    }
+*/
     void append(int val) {
         if (head == nullptr) {
-            head = new Node(val);
+            head = new Node(val, head);
             size ++;
             return;
         }        
@@ -38,11 +54,12 @@ class  CircLinkedList{
         current = head;
         while (current->next != head) {
             current = current->next;
+            std::cout << current->value << std::endl;
         }
     
         // Link new node to end of list
-        current->next = new Node(val);
-        (current->next)->next = head;
+        current->next = new Node(val, head);
+        //current->next->next = head;
         size ++;
     }
     int& operator[](int index){
@@ -55,8 +72,52 @@ class  CircLinkedList{
         }
         return current->value;
     }
+    void print() {
+        Node* current = head;
+        std::cout << "[";
+        while (current->next != head) {
+            std::cout << current->value;
+            std::cout << ", ";
+            current = current->next;
+        }
+        std::cout << current->value << "]" << std::endl;
+    }
+    ~CircLinkedList() {
+        Node* current;
+        Node* next;
+    
+        current = head;
+    
+        while (current != head) {
+            next = current->next;
+            delete current;
+            current = next;
+        }
+    }
+/*
+    std::vector<int> josephus_sequence(int k){
+        int i = 1;
+        int *x;
+        x = new int[size];
+        Node* current;
+        while(i<size){
+            for (int j=1; j<=k; j++){
+                current = current->next;
+            }
+            x[i] = current->value;
+            i ++;
+        }
+        delete[] x;
+        return x;
+    }
+*/
 };
 
 int main(){
+    CircLinkedList clist;
+    //clist.append(0);
+    //clist.append(2);
+    clist.append(4);
+    clist.print();
     return 0;
 }
